91f1a3feee043859869e0a06661698a376b309de
Kunaal-byte <133675180+Kunaal-byte@users.noreply.github.com>
2023-05-15 22:09:28 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,98 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+
+
+
+let map: google.maps.Map, infoWindow: google.maps.InfoWindow;
+
+
+
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+  const myLatLng = {  lat: -34.397, lng: 150.644};
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    draggable:true,
+    title:"Drag me!"
+});
+
+  const locationButton = document.createElement("button");
+  const newsweb = document.createElement("button");
+
+  locationButton.textContent = "Current Location";
+  locationButton.classList.add("custom-map-control-button");
+
+  newsweb.textContent="Find News"
+  newsweb.classList.add()
+
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(newsweb);
+
+  newsweb.addEventListener("click",() =>{
+    window.open('https://8080-googlemaps-jssamples-4q6906jybwf.ws-us97.gitpod.io/?q=all&pageno=1','_blank')
+  })
+
+
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          
+          map.setCenter(pos);
+          marker.setPosition(pos);
+          marker.setMap( map );
+          marker.setPosition( new google.maps.LatLng( position.coords.latitude,position.coords.longitude ) );
+          map.panTo( new google.maps.LatLng( position.coords.latitude,position.coords.longitude ) );
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter()!);
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter()!);
+    }
+  });
+}
+
+
+function handleLocationError(
+  browserHasGeolocation: boolean,
+  infoWindow: google.maps.InfoWindow,
+  pos: google.maps.LatLng
+) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+
+
+export{}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
+
---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const path = require('path')
+const axios = require('axios')
+const app = express()
+const port = 8080
+const {join} = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile("newsindex.html", { root: join(__dirname) })
+})
+
+app.get('/api', async (req, res) => {
+    console.log(req._parsedUrl.query)
+    let url = "https://newsapi.org/v2/everything?" + req._parsedUrl.query
+    let r = await axios(url)
+    let a = r.data
+    res.send(a)
+})
+console.log("start1")
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
