7a599f42b1d70813300dd2bd3cd02d8660fd65a4
Kunaal-byte <133675180+Kunaal-byte@users.noreply.github.com>
2023-05-16 00:14:17 +0530

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,91 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+let map, infoWindow
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6
+  })
+  infoWindow = new google.maps.InfoWindow()
+  const myLatLng = { lat: -34.397, lng: 150.644 }
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    draggable: true,
+    title: "Drag me!"
+  })
+
+  const locationButton = document.createElement("button")
+  const newsweb = document.createElement("button")
+
+  locationButton.textContent = "Current Location"
+  locationButton.classList.add("custom-map-control-button")
+
+  newsweb.textContent = "Find News"
+  newsweb.classList.add()
+
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton)
+  map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(newsweb)
+
+  newsweb.addEventListener("click", () => {
+    window.open(
+      "https://8080-kunaalbyte-kunaalbytegi-rlyyygydhb6.ws-us97.gitpod.io/?q=all&pageno=1",
+      "_blank"
+    )
+  })
+
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+
+          map.setCenter(pos)
+          marker.setPosition(pos)
+          marker.setMap(map)
+          marker.setPosition(
+            new google.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude
+            )
+          )
+          map.panTo(
+            new google.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude
+            )
+          )
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter())
+        }
+      )
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter())
+    }
+  })
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos)
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  )
+  infoWindow.open(map)
+}
+
+export {}
+
+window.initMap = initMap
